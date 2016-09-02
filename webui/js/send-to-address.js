8796ba7c86148357ff4c2ab0eaf5ccf707fccba8
Iamwebguru <Iamwebguru5@gmail.com>
2016-09-03 00:41:24 +0530

    added new file send-to-address.js
    Added sliced JS code into this new file for send to address component.

---
+++ webui/js/send-to-address.js
@@ -0,0 +1,48 @@
+/*
+Send Button to send coin another address Js
+*/
+
+$(document).ready(function(){
+    $("#sendCoinModal, #confirmation, #sending, #transactionsent").dialog({
+        modal: true,
+        autoOpen: false,
+        width: '800',
+        height: 'auto',
+        create: function (event, ui){
+            $(".ui-widget-header").hide();
+        }
+    });
+
+    $("#send").click(function (){
+        $('#sendCoinModal').dialog('open');
+    });
+
+    $(".close").on("click", function(){
+        $("#sendCoinModal").dialog('close');
+        $('#confirmation').dialog('close');
+    });
+      
+    $("#nextsend").click(function (){
+        $("#sendCoinModal").dialog('close');
+        $('#confirmation').dialog('open');
+    });
+
+    $(".back").click(function (){
+        $("#sendCoinModal").dialog('open');
+        $('#confirmation').dialog('close');
+    });
+
+    $("#sendingModal").click(function (){
+        $("#confirmation").dialog('close');
+        $('#sending').dialog('open');
+    });
+
+    $("#transactionsentmodal").click(function (){
+        $("#sending").dialog('close');
+        $('#transactionsent').dialog('open');
+    });
+
+    $("#transactioncomplete").click(function () {
+        $('#transactionsent').dialog('close');
+    });
+});
