26a19f5da162d782e63386f2e5c168b75f65061b
pbca26 <pbca26@gmail.com>
2016-10-01 06:02:27 -0400

    settings update

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,12 @@
 var settings = {
   iguanaPort: '7778',
-  proxy: 'http://localhost:1337/' // https://github.com/gr2m/CORS-Proxy
+  proxy: 'http://localhost:1337/', // https://github.com/gr2m/CORS-Proxy
+  ratesUpdateTimeout: 15, // sec
+  decimalPlacesCoin: 1,
+  decimalPlacesCurrency: 2,
+  decimalPlacesTxUnit: 5,
+  dashboardUpdateTimout: 15, // sec
+  defaultCurrency: 'USD',
+  portPollUpdateTimeout: 60, // sec
+  defaultSessionLifetime: 7200 // sec
 };
