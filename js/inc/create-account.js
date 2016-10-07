f4e48e92f1c1bebc17af2489d61076386d1b5ac6
pbca26 <pbca26@gmail.com>
2016-10-07 08:58:11 -0400

    js includes update

--- js/inc/create-account.js
+++ js/inc/create-account.js
@@ -9,7 +9,7 @@ document.write('\x3Cscript type=\"text/javascript\" src=\"js/libs/crypto/wordlis
                '\x3Cscript type=\"text/javascript\" src=\"js/settings.js\">\x3C/script>' +
                '\x3Cscript type=\"text/javascript\" src=\"js/dev.js\">\x3C/script>' +
                '\x3Cscript type=\"text/javascript\" src=\"js/supported-coins-list.js\">\x3C/script>' +
-               '\x3Cscript type=\"text/javascript\" src=\"js/api.js\">\x3C/script>' +
+               '\x3Cscript type=\"text/javascript\" src=\"js/api/api.js\">\x3C/script>' +
                '\x3Cscript type=\"text/javascript\" src=\"js/localstorage.js\">\x3C/script>' +
                '\x3Cscript type=\"text/javascript\" src=\"js/helpers.js\">\x3C/script>' +
-               '\x3Cscript type=\"text/javascript\" src=\"js/auth.js\">\x3C/script>');
+               '\x3Cscript type=\"text/javascript\" src=\"js/auth/auth.js\">\x3C/script>');
