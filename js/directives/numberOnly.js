b0151c3e4d76b150d883898ef3903209b6cb5484
pbca26 <pbca26@gmail.com>
2016-11-12 13:14:10 -0500

    updated numberOnly directive

--- js/directives/numberOnly.js
+++ js/directives/numberOnly.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('IguanaGUIApp')
-.directive('numberOnly', ['$document', function($document) {
+.directive('numberOnly', ['$document', '$filter', function($document, $filter) {
   return {
     restrict: 'A',
     require: '?ngModel',
@@ -13,7 +13,15 @@ angular.module('IguanaGUIApp')
 
         if (inputValue.match(/\./g) && inputValue.match(/\./g).length > 1) { // allow only one dot
           var inputValSplit = inputValue.split('.');
+
           transformedInput = inputValSplit[0] + '.' + inputValSplit[1];
+        }
+
+        var inputValSplit = transformedInput.split('.');
+
+        if ((inputValSplit[0].match(/0/g) && inputValSplit[0].match(/0/g).length > 1) || // disallow leading zero on the left side
+            (Number(inputValSplit[0][0]) === 0 && Number(inputValSplit[0][1]) > 0)) {
+          transformedInput = '0.' + inputValSplit[1];
         }
 
         if (transformedInput !== inputValue) {
