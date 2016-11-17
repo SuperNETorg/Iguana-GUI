ec2fcf9265e5f5f5d897874ce11c7d9a493af997
pbca26 <pbca26@gmail.com>
2016-11-17 10:26:51 -0500

    updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     injectPartials = require('gulp-inject-partials'),
     del = require('del'),
+    rimraf = require('gulp-rimraf');
     gutil = require('gulp-util'),
     sass = require('gulp-sass');
 
@@ -32,27 +33,27 @@ gulp.task('copyFonts', ['cleanFonts'], function() {
 });
 
 gulp.task('cleanCSS', function() {
-  return del(['compiled/dev/css']);
+ return gulp.src('compiled/dev/css/*', { read: false })
+            .pipe(rimraf());
 });
 
 gulp.task('cleanJS', function() {
-  return del(['compiled/dev/js']);
+ return gulp.src('compiled/dev/js/*', { read: false })
+            .pipe(rimraf());
 });
 
 gulp.task('cleanFonts', function() {
-  return del(['compiled/dev/fonts']);
+ return gulp.src('compiled/dev/fonts/*', { read: false })
+            .pipe(rimraf());
 });
 
 gulp.task('cleanIndex', function() {
-  return del(['compiled/dev/index.html']);
-});
-
-gulp.task('clean', function() {
-  return del(['compiled/dev']);
+ return gulp.src('compiled/dev/index.html', { read: false })
+            .pipe(rimraf());
 });
 
 gulp.task('default', function() {
-  return gutil.log('Gulp is running!');
+  return gutil.log('Run gulp dev to build dev version');
 });
 
 var paths = {
@@ -67,4 +68,4 @@ gulp.task('watch:dev', function() {
   gulp.watch(paths.styles, ['scss']);
 });
 
-gulp.task('dev', ['index', 'cleanCSS', 'copyFonts', 'copyJS', 'scss', 'watch:dev']);
+gulp.task('dev', ['cleanCSS', 'index', 'copyFonts', 'copyJS', 'scss', 'watch:dev']);
