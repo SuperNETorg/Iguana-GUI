8498b82b5e7eadec6285229bc52d02d06c6f7d69
arkadig <ghukasyan.ark@gmail.com>
2016-11-30 09:02:38 +0400

    Changed chrome app description and icon

--- chrome-app/background.js
+++ chrome-app/background.js
@@ -1,8 +1,8 @@
  chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create("app/index.html", {
     'outerBounds': {
-      'width': 700,
-      'height': 500
+      'width': window.screen.availWidth,
+      'height': window.screen.availHeight
     }
   });
 });
