7f19631c481bc72f5007facf2f6b6241c9072f21
pbca26 <pbca26@gmail.com>
2016-12-13 16:20:22 +0300

    missing electron inject code

---
+++ electron-patch.js
@@ -0,0 +1,12 @@
+<script>
+  window.angular = require('angular');
+</script>
+<script>
+  if (typeof module === 'object') {
+    window.module = module;
+    module = undefined;
+  }
+</script>
+<script>
+  if (window.module) module = window.module;
+</script>
