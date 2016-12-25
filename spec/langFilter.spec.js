616d5d913d5a714f7e391f4ad66fbff3e2c86385
pbca26 <pbca26@gmail.com>
2016-12-25 19:16:48 +0300

    updated langFilter spec

--- spec/langFilter.spec.js
+++ spec/langFilter.spec.js
@@ -13,11 +13,16 @@ describe('lang filter test', function() {
       expect($filter('lang')).toBeDefined();
     });
 
-    it('shoud display warning message', function() {
+    it('shoud display 2 warning messages', function() {
       var filterTest = $filter('lang')('LOGIN.TEST123');
       expect(filterTest).toEqual('{{ LOGIN.TEST123 }}');
       filterTest = $filter('lang')('TEST');
       expect(filterTest).toEqual('{{ TEST }}');
+    });
+
+    it('shoud display lang placeholder', function() {
+      var filterTest = $filter('lang')('PAGE.LOGIN');
+      expect(filterTest).toEqual('Login');
     });
   });
 });
