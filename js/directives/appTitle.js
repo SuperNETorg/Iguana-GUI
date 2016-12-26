f4bf14fcd73206088dbd1b3b3c78aa88cdd0307f
pbca26 <pbca26@gmail.com>
2016-12-26 12:32:00 +0300

    refactored appTitle setter function

--- js/directives/appTitle.js
+++ js/directives/appTitle.js
@@ -3,21 +3,21 @@
 angular.module('IguanaGUIApp')
 .directive('appTitle', [
   '$rootScope',
-  '$timeout',
   '$filter',
-  function($rootScope, $timeout, $filter) {
+  function($rootScope, $filter) {
     return {
       restrict: 'A',
       link: function(scope, element) {
         var listener = function(event, toState) {
+          toState = toState ? toState : $rootScope.toState; // temp bit for unit test compat
           var title = $filter('lang')('IGUANA.APP_TITLE');
 
           if (toState.data && toState.data.pageTitle)
             title = $filter('lang')('IGUANA.APP_TITLE') + ' / ' + $filter('lang')(toState.data.pageTitle);
 
-          $timeout(function() {
+          (function() {
             element.text(title);
-          }, 0, false);
+          })();
         };
 
         $rootScope.$on('$stateChangeStart', listener);
